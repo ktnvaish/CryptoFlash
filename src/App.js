7e1c75f43fb195b587c13e1c94c79c641be8f16f
Ketan Vaish <ktnvaish@gmail.com>
2021-07-25 01:18:09 +0530

    Completed Cryptotracker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import Coin from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
-
+  const [search, setSearch] = useState("");
   useEffect(() => {
     axios
       .get(
@@ -17,10 +18,40 @@ function App() {
       .catch((error) => console.log(error));
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="coin-app">
       <div className="coin-search">
-        <h1 className="coin-text">Search a Currency</h1>
+        <h1 className="coin-text">Get a CryptoFlash</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              image={coin.image}
+              symbol={coin.symbol}
+              price={coin.current_price}
+              volume={coin.total_volume}
+              priceChange={coin.price_change_percentage_24h}
+              marketcap={coin.market_cap}
+            />
+          );
+        })}
       </div>
     </div>
   );
